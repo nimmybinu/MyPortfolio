e12033af8b0e9a6fdbe9223bfdb7160ba4d24ad7
nimmy <nimmimm2014@gmail.com>
2024-02-06 18:45:07 +0530

    home section completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="bg-black w-full h-screen ">
+      {/* container */}
+      <div className="max-w-[1000px] h-full flex flex-col justify-center mx-auto">
+        <p className="text-green-400">Hi, My name is,</p>
+        <h2 className="text-gray-200 text-3xl sm:text-7xl font-bold">Nimmy</h2>
+        <h3 className="text-3xl sm:text-7xl font-bold text-gray-400">
+          MERN stack developer.
+        </h3>
+        <p className="text-gray-400 py-4 max-w-[700px]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          quae libero ad debitis dolorem, fugiat sunt quidem, veniam, suscipit
+          quis unde voluptate? Recusandae, amet nihil? Deserunt alias repellat
+          nemo amet?
+        </p>
+        <div>
+          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:border-green-400 hover:bg-green-400">
+            My Works{" "}
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3" />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import logo from "../assets/logo.jpg";
-import { FaBars, FaTimes } from "react-icons/fa";
+import {
+  FaBars,
+  FaTimes,
+  FaLinkedin,
+  FaGithub,
+  FaFacebook,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 const Navbar = () => {
   const [nav, setNav] = useState(true);
   const handleClick = () => setNav(!nav);
@@ -21,7 +29,7 @@ const Navbar = () => {
 
       {/* hamburger */}
       <div className="md:hidden z-10" onClick={handleClick}>
-        {nav?<FaBars />:<FaTimes/>}
+        {nav ? <FaBars /> : <FaTimes />}
       </div>
       {/* mobile menu */}
       <ul
@@ -38,7 +46,42 @@ const Navbar = () => {
         <li className="py-6 text-4xl">Contact</li>
       </ul>
       {/* icons */}
-      <div className="hidden"></div>
+      <div className="hidden lg:flex flex flex-col fixed top-[35%] left-0">
+        <ul>
+          <li className="flex justify-between items-center w-[160px] h-[60px] ml-[-100px] hover:ml-[-10px] duration-100 bg-blue-600">
+            <a
+              href="/"
+              className="flex justify-between items-center text-gray-300 w-full"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="flex justify-between items-center w-[160px] h-[60px] ml-[-100px] hover:ml-[-10px] duration-100 bg-gray-600">
+            <a
+              href="/"
+              className="flex justify-between items-center text-gray-300 w-full"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="flex justify-between items-center w-[160px] h-[60px] ml-[-100px] hover:ml-[-10px] duration-100 bg-blue-600">
+            <a
+              href="/"
+              className="flex justify-between items-center text-gray-300 w-full"
+            >
+              Mail <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="flex justify-between items-center w-[160px] h-[60px] ml-[-100px] hover:ml-[-10px] duration-100 bg-gray-600">
+            <a
+              href="/"
+              className="flex justify-between items-center text-gray-300 w-full"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
